b66dffed180da2562b2552c730315c02b7795c0d
John Connolly <JTConnolly82@gmail.com>
2019-08-27 17:55:10 -0600

    created Todos component, imported it to App, cleaned up index to remove serviceworker & index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './App.css';
+import Todos from './Components/Todos';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <h1>Todos</h1>
+        <Todos />
       </div>
     );
   }
