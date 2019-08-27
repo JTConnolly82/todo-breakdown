68aa628a5c467bd67fc99e05f32ba051acdc312a
John Connolly <JTConnolly82@gmail.com>
2019-08-27 17:58:45 -0600

    imported uuid, added state w todos to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import './App.css';
 import Todos from './Components/Todos';
+import uuid from 'uuid';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        title: 'first one',
+        id: uuid.v4(),
+        completed: false
+      },
+      {
+        title: 'another thing todo',
+        id: uuid.v4(),
+        completed: false
+      },
+    ]
+  }
   render() {
     return (
-      <div className="App">
+      <div>
         <h1>Todos</h1>
         <Todos />
       </div>
