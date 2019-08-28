15135c38d50ea7de817f62b0b45e55b3c57b07cd
John Connolly <JTConnolly82@gmail.com>
2019-08-27 18:23:47 -0600

    added style to TodoItem component, textDecoration conditional on state of todo.completed

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
 class TodoItem extends Component {
+
+  getStyle = () => {
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px black dotted',
+      textDecoration: this.props.todo.completed ?
+      'line-through' : 'none'
+    }
+  }
+
+
   render() {
     return (
-        <div>
+        <div style={this.getStyle()}>
           <h3>
             {this.props.todo.title}
           </h3>
