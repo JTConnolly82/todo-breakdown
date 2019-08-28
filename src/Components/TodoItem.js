2fa4518a984bc0602d7c1826a71da1c434a8d526
John Connolly <JTConnolly82@gmail.com>
2019-08-27 18:09:07 -0600

    added TodoItem component, refactored Todos comp to use TodoItem instead of h3

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return (
+        <div>
+          <h3>
+            Hi from todoitem
+          </h3>
+        </div>
+    );
+  }
+}
+
+export default TodoItem;
