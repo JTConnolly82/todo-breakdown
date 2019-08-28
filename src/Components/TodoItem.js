ec2aadce323f52c6b66ee51b491aeb36c2848243
John Connolly <JTConnolly82@gmail.com>
2019-08-27 18:54:57 -0600

    delete button added to todoItem w style no functionality

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class TodoItem extends Component {
 
+
   getStyle = () => {
     return {
       background: '#f4f4f4',
@@ -13,7 +14,6 @@ class TodoItem extends Component {
   }
 
 
-
   render() {
     return (
         <div style={this.getStyle()}>
@@ -21,10 +21,23 @@ class TodoItem extends Component {
             <input type='checkbox' 
                    onChange={this.props.handleChange.bind(this, this.props.todo.id)} />{' '}
             {this.props.todo.title}
+            <button style={btnStyle}>delete</button>
           </h3>
         </div>
     );
   }
 }
+
+
+let btnStyle = {
+  background: 'red',
+  color: 'white',
+  border: 'none',
+  padding: '5px',
+  borderRadius: '20%',
+  float: 'right'
+}
+
+
 
 export default TodoItem;
